29e384b41ae9c3b76e368eaaf613003f661748e3
Ascilius <jasonkim050@gmail.com>
2023-10-27 13:35:41 -0400

    Added README.md

--- WaveDriver.java
+++ WaveDriver.java
@@ -3,35 +3,6 @@ import java.awt.Toolkit;
 
 import javax.swing.JFrame;
 
-/*
-Version History:
-v.4
- - movable camera (WASD)
- 	- zoom
- - pause
- - wave stuff
-    - hover for wave information
- 	- drag waves around
-v.3
- - key controls
-	- view
-	   - invert
-	   - grid
-	   - hide
-    - stretch
-    - scenarios
-       - constructive interference
-       - destructive interference
-       - superposition
-       - random
-v.2
- - multiple waves
- - wave superposition
-v.1
- - basic engine creation
- - preliminary wave tests
- */
-
 public class WaveDriver {
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("Wave Generator");
